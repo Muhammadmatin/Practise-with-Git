02b545132f22ea6e1bbae58bcb7597e95f2ca515
Muhammadmatin <Muhammadmatindavlatov@gmail.com>
2023-06-13 11:26:07 +0300

    first commit

---
+++ Practise.js
@@ -0,0 +1,21 @@
+//1
+// function decloration(a,b) {
+//     return a+b
+// }
+// console.log(decloration(2,4));
+
+
+//2
+// let expresion=function (a,b){
+//     return a+b
+// }
+// console.log(expresion(2,5));
+
+
+//3
+// let functionexpression2 = (a,b) =>{
+//     return a+b
+// }
+// console.log(functionexpression2(5,10));
+
+
